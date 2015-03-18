4e04878797d799bd20a09b1583ee13d174702b96
Jeremiah Currier <jcurrier@zendesk.com>
2015-03-18 11:33:29 -0700

    Adding demo video and logging

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 (function() {
-
   return {
     events: {
-      'app.created':'exampleFunction'
+      'app.created':'demoFunction'
     },
-
-    exampleFunction: function() {
-
-      // this.store('key') // getter
-      // this.store('key', dataObject) // setter
-
-      // Set variable exampleVariable to object with {key}:{value} pair
-      var exampleVariable = {
-          'exampleKey': 'exampleValue'
-      };
-
-      this.store('exampleKey', exampleValue); // set value of 'exampleKey' to 'exampleValue' - save to localStorage
-      console.log(this.store('exampleKey')); // get value of 'exampleKey' - print to console
-      
+    demoFunction: function() {
+      console.log('--- demoFunction (start) ---');
+      var exampleObject = { 'exampleKey': 'exampleValue' }; // Set variable exampleObject to object
+      this.store('exampleObject', exampleObject); // set value of 'exampleObject' to 'exampleValue' - save to localStorage
+      console.log('\'exampleObject\':');
+      console.log(this.store('exampleObject')); // print 'exampleObject' object to browser console
+      var exampleString = 'hello world'; // Set variable exampleVariable to string
+      this.store('exampleString', exampleString); // set value of 'exampleVariable' to 'exampleValue' - save to localStorage
+      console.log('\'exampleString\':');
+      console.log(this.store('exampleString')); // print 'exampleVariable' string to browser console
+      console.log('--- demoFunction (end) ---');
     }
   };
-
 }());
